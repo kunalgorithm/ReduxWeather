67254d824949ce8fac757f48a97d135b832cdb9b
Kunal <kshah239@gmail.com>
2018-08-19 11:28:20 +0700

    added files

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+// Open Weather API
+const API_KEY = "16034c5104346b1117dd3354057a2bbc";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
+
+
